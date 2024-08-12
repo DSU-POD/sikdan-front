89e88d4aa218ec45830977cfaca87c63ffa02f67
Sangho <68239144+sssangho@users.noreply.github.com>
2024-08-12 14:08:51 +0900

    피드에 임의의 정보 입력

--- src/pages/main/feed/index.jsx
+++ src/pages/main/feed/index.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { FeedComponent } from "@/components/feed-component";
+import { Avatar } from '@radix-ui/react-avatar';
 
 export default function Feed({ posts }) {
   return (
@@ -11,23 +12,35 @@ export default function Feed({ posts }) {
 
 export async function getServerSideProps(context) {
   const { page = 1 } = context.query; 
-  const type = context.query.type || 'expert'; 
+  const type = context.query.type ?? 'expert'; 
 
-  try {
-    const response = await axios.get(`http://3.34.53.152:3001/feed/list/${page}?type=${type}`);
-    const posts = response.data;
+  const posts = [
+    {
+      avatar: "",
+      username: "Lee Sangho",
+      timeAgo: "3분 전",
+      image: "",
+      caption: "오늘의 식단",
+    },
+    {
+      avatar: "",
+      username: "SimSim",
+      timeAgo: "1시간 전",
+      image: "",
+      caption: "퇴근하고 싶다",
+    },
+    {
+      avatar: "",
+      username: "JEJU",
+      timeAgo: "1일 전",
+      image: "",
+      caption: "제주 금귤",
+    },
+  ];
 
-    return {
-      props: {
-        posts,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching posts:', error.message);
-    return {
-      props: {
-        posts: [],
-      },
-    };
-  }
+  return {
+    props: {
+      posts,
+    },
+  };
 }
