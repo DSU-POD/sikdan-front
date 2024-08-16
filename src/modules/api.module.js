0d36019cb90d0ab2ffc10382138e800d0fe15a95
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-16 19:25:40 +0900

    feat: application type formdata 추가

--- src/modules/api.module.js
+++ src/modules/api.module.js
@@ -23,3 +23,45 @@ api.interceptors.response.use(
     }
   }
 );
+
+export const formDataApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  validateStatus: (status) => {
+    if (status === 401 || status === 400) {
+      Promise.reject(status);
+      return false;
+    }
+    return true;
+  },
+});
+
+formDataApi.interceptors.request.use((request) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return Promise.reject(401);
+  }
+
+  request.headers.Authorization = `Bearer ${token}`;
+
+  return request;
+});
+
+formDataApi.interceptors.response.use(
+  (response) => {
+    return Promise.resolve(response.data);
+  },
+  (error) => {
+    if (error.response) {
+      const { status, data } = error.response;
+
+      if (status >= 400 && status < 600) {
+        return Promise.reject(data);
+      }
+      return false;
+    }
+  }
+);
