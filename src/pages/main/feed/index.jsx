53e329fd383abe2fe96029a2eaa79f6bf093a783
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-02 15:48:38 +0900

    fix: typo

--- src/pages/main/feed/index.jsx
+++ src/pages/main/feed/index.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { FeedComponent } from "@/components/feed-component";
 
 export default function Feed({ posts }) {
@@ -7,24 +6,4 @@ export default function Feed({ posts }) {
       <FeedComponent posts={posts} />
     </>
   );
-}
-
-export async function getServerSideProps() {
-  try {
-    const response = await axios.get('http://3.34.53.152:3001');
-    const posts = response.data;
-
-    return {
-      props: {
-        posts,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    return {
-      props: {
-        posts: [],
-      },
-    };
-  }
 }
