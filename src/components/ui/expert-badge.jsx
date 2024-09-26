01f0b16d0bc05ca6cf9da6dcdbbe99cbf4eec82c
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-26 20:34:41 +0900

    FEAT: 전문가 뱃지 추가.

---
+++ src/components/ui/expert-badge.jsx
@@ -0,0 +1,32 @@
+export default function ExpertBadgeComponent() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="none"
+        className="size-6"
+        data-tooltip-target="tooltip-right"
+        data-tooltip-placement="right"
+      >
+        <circle cx="12" cy="12" r="12" fill="#72a555" />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          stroke="white"
+          strokeWidth={1.5}
+          d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+        />
+      </svg>
+      <div
+        id="tooltip-right"
+        role="tooltip"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+      >
+        Tooltip on right
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
+    </>
+  );
+}
