5d865b9465ad845b4998562535726062a334822d
pgkwon1@naver.com <pgkwon1@naver.com>
2024-09-26 20:34:12 +0900

    REFACTOR: api 요청 경로 및 next config rewrites 룰 추가.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ghchart.rshah.org",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/modules/api.module.js
+++ src/modules/api.module.js
@@ -2,7 +2,7 @@ import { showToast } from "@/components/layout/toast";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `/api/`,
   validateStatus: (status) => {
     if (status === 401) {
       showToast("다시 로그인 후 시도해주세요.");
