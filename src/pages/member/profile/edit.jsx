f2bcb9611d3e2616fe441f0c2fd35bb6d22021e1
창용 <dndyd4323@naver.com>
2024-10-07 14:30:57 +0900

    FEAT : 음식 검색 기능 추가

--- src/pages/member/profile/edit.jsx
+++ src/pages/member/profile/edit.jsx
@@ -3,3 +3,11 @@ import ProfileEditComponent from "@/components/profile-edit";
 export default function ProfileEdit() {
   return <ProfileEditComponent />;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      isBeforeLogin: true,
+    },
+  };
+};
