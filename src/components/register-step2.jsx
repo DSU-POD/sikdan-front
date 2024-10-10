b052fdfc6ceb293840367454e8f771ae17b9d9e4
Sangho <68239144+sssangho@users.noreply.github.com>
2024-10-10 20:40:33 +0900

    feat: icon파일 추가

--- src/components/register-step2.jsx
+++ src/components/register-step2.jsx
@@ -34,6 +34,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setRegisterStep2Data } from "@/store/reducers/member.reducer";
 import { useRouter } from "next/router";
 import { showToast } from "./layout/toast";
+import ArrowLeftIcon from "./icon/arrowlefticon";
 
 export default function RegisterStep2Component() {
   const [gender, setGender] = useState("");
@@ -162,25 +163,5 @@ export default function RegisterStep2Component() {
         </Card>
       </div>
     </>
-  );
-}
-
-function ArrowLeftIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m12 19-7-7 7-7" />
-      <path d="M19 12H5" />
-    </svg>
   );
 }
